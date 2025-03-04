721f3781f9c7850cd7dfd8eb056067f2973b3221
Joshua Sonnen <sonnenj@wwu.edu>
2025-03-04 12:24:01 -0800

    pushing new graph set

--- plotting.py
+++ plotting.py
@@ -1,5 +1,4 @@
 import os
-import re
 import pickle
 import pandas as pd
 import argparse
@@ -7,29 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-def load_pickle_files(directory):
-    """Loads all pickle files from a folder. Then computes confidence 
-    intervals for the test accuracy across all epochs."""
-    
-    names = []
-    plotting_CI = []
-    for filename in os.listdir(directory):
-        names.append(filename)
-        if filename.endswith(".pkl"):
-            file_path = os.path.join(directory, filename)
-            with open(file_path, "rb") as f:
-                data = pickle.load(f)
-                matrix = np.array(data['performance']['test_accuracy'])
-
-                # use seaborn to compute a non-parametric CI for each epoch
-                CI = []
-                for epoch in matrix:
-                    boot_ci = sns.algorithms.bootstrap(epoch, func=np.mean, n_boot=1000, ci=95)
-                    ci_matrix = np.array([np.mean(epoch), np.percentile(boot_ci, 2.5), np.percentile(boot_ci, 97.5)])
-                    CI.append(ci_matrix)
-                CI = np.array(CI)
-                plotting_CI.append(CI)
-    return np.array(plotting_CI), names
 
 def generate_label(hyperparams):
     """Generate a label based on 'projection', 'learnable_gbs', and 'scale' rules."""
@@ -99,32 +75,59 @@ def plot_test_accuracies(data_list):
     plt.grid(True)
     plt.show()
 
+def load_pickle_files(directory):
+    """Loads all pickle files from a folder. Then computes confidence 
+    intervals for the test accuracy across all epochs."""
+    
+    names = []
+    plotting_CI = []
+    for filename in os.listdir(directory):
+        names.append(filename)
+        if filename.endswith(".pkl"):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, "rb") as f:
+                data = pickle.load(f)
+                matrix = np.array(data['performance']['test_accuracy']).T
+
+                # use seaborn to compute a non-parametric CI for each epoch
+                CI = []
+                for epoch in matrix:
+                    boot_ci = sns.algorithms.bootstrap(epoch, func=np.mean, n_boot=1000, ci=95)
+                    ci_matrix = np.array([np.mean(epoch), np.percentile(boot_ci, 2.5), np.percentile(boot_ci, 97.5)])
+                    CI.append(ci_matrix)
+                CI = np.array(CI)
+                plotting_CI.append(CI)
+    return np.array(plotting_CI), names
+
 def CI_plots(matrix, names, outfile= "figure.png"):
-    # TODO: change to accept matrix as an input
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 
     for i, table in enumerate(matrix):
         x = np.arange(1, table.shape[0]+1)
-        sns.lineplot(x=x, y=table[:,0], label= names[i].split("-scale")[0], marker='o')
+        label = names[i].split("-")[3].strip("[")
+        sns.lineplot(x=x, y=table[:,0], label= label, marker='')
         plt.fill_between(x, table[:,1], table[:,2], alpha=0.2)
         
-    plt.title('Fastfood performance compared with different learning rates')
+    title = names[0].split("-")[0]
+    plt.title(f'{title} Nonlearnable performance')
     plt.xlabel('Epochs')
     plt.ylabel('Test Accuracy')
     plt.xticks(x)
+    # plt.ylim(82, 90)
     plt.legend()
     plt.savefig(outfile, dpi=300)
     plt.show()
 
 
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot test accuracy from pickle files.")
-    parser.add_argument('paths', type=str, nargs='+', help="Path(s) to pickle files or directories.")
+    parser.add_argument('--filepath', type=str, nargs='+', help="Path(s) to pickle files or directories.")
     
     args = parser.parse_args()
-    
-    data_list = CI_plots(args.paths)
-    
-if __name__ == "__main__":
-    foo, names = load_pickle_files("learnables")
-    CI_plots(foo, names)
+
+    # Get all subdirectories
+    subdirs = [f.path for f in os.scandir(*args.filepath) if f.is_dir()]
+
+    for dir in subdirs:
+        foo, names = load_pickle_files(dir)
+        CI_plots(foo, names, outfile= dir + "plot.png")
