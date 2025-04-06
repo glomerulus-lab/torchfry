9d2b085af41f51bcf567f24bc462df5cba3436da
Joshua Sonnen <sonnenj@wwu.edu>
2025-04-05 22:56:31 -0700

    Swiss army knife plotting

--- plotting.py
+++ plotting.py
@@ -18,8 +18,16 @@ def parse_json(filename, yaxis):
         CI = []
         names = []
         for trial in data:
+            # Filter out RKS runs
+            if trial["Projection Layer"] != "RKS_Layer":
+                continue
+
             # Parse a name for the trial
-            name = f"{trial['Projection Layer']} LR={trial['Projection Arguments']['lr']}"
+            # name = f"{trial['Projection Layer']} LR={trial['Projection Arguments']['lr']}"
+            if trial['Projection Arguments']['learn_G']:
+                name = f"Learnable, Scale={trial['Projection Arguments']['scale']}"
+            else:
+                name = f"Non-Learnable, Scale={trial['Projection Arguments']['scale']}"
             names.append(name)
 
             # Create a 95% confidence interval for the mean.
@@ -46,14 +54,16 @@ def plot(matrix, names, outfile=None):
     for i, table in enumerate(matrix):
         x = np.arange(1, table.shape[1]+1)
         sns.lineplot(x=x, y=table[0,:], marker='', label= names[i])
-        plt.fill_between(x, table[1,:], table[2,:], alpha=0.3)
+        if args.CI == "True":
+            plt.fill_between(x, table[1,:], table[2,:], alpha=0.3)
 
-    plt.title(f'Performance')
+    plt.title(args.filepath)
     plt.xlabel('Epochs')
-    plt.ylabel('Test Accuracy')
+    plt.ylabel(args.yaxis)
     plt.xticks(x)
-    # plt.ylim(82, 90)
     plt.legend()
+    if args.ylim is not None:
+        plt.ylim(args.ylim[0], args.ylim[1])
     if outfile != None:
         plt.savefig(outfile, dpi=300)
     plt.show()
@@ -61,8 +71,10 @@ def plot(matrix, names, outfile=None):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot test accuracy from pickle files.")
-    parser.add_argument('--filepath', type=str, nargs='+', help="Path(s) to pickle files or directories.")
+    parser.add_argument('--filepath', type=str, default="Results/ff_rks_l_lr.json", help="Path to json file.")
     parser.add_argument('--yaxis', type=str, default="Test Accuracies", help="What to plot on the y-axis.")
+    parser.add_argument('--CI', type=str, default="True", help="Plot confidence intervals or not.")
+    parser.add_argument('--ylim', nargs='+', default=None, type=int, help='Upper and Lower limits for the plot.')
     args = parser.parse_args()
 
-    parse_json("Results/ff_rks_l_lr.json", args.yaxis)
+    parse_json(args.filepath, args.yaxis)
