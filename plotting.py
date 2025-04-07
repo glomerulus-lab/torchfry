f3c7e5eb499f71db384223a02bf2b380b30db60e
Joshua Sonnen <sonnenj@wwu.edu>
2025-04-06 19:50:54 -0700

    updates to plotting.py

--- plotting.py
+++ plotting.py
@@ -13,22 +13,27 @@ def parse_json(filename, yaxis):
     with open(filename, 'r') as file:
         data = json.load(file)
         alpha = 0.05
-
+        
         # initize lists to store confidence intervals and parsed names.
         CI = []
         names = []
         for trial in data:
-            # Filter out RKS runs
-            if trial["Projection Layer"] != "RKS_Layer":
+            # Filter out RKS or FF runs
+            if args.ff and trial["Projection Layer"] == "FastFood_Layer":
+                continue
+            if args.rks and trial["Projection Layer"] == "RKS_Layer":
                 continue
 
-            # Parse a name for the trial
-            # name = f"{trial['Projection Layer']} LR={trial['Projection Arguments']['lr']}"
-            if trial['Projection Arguments']['learn_G']:
-                name = f"Learnable, Scale={trial['Projection Arguments']['scale']}"
-            else:
-                name = f"Non-Learnable, Scale={trial['Projection Arguments']['scale']}"
-            names.append(name)
+            # Parse a name for the trial. Different for each filepath
+            if args.filepath == "Results/learnrate.json":
+                name = f"{trial['Projection Layer']} LR={trial['Projection Arguments']['lr']}"
+                names.append(name)
+            elif args.filepath == "Results/scale.json":
+                if trial['Projection Arguments']['learn_G']:
+                    name = f"{trial["Projection Layer"]} Learnable, Scale={trial['Projection Arguments']['scale']}"
+                else:
+                    name = f"{trial["Projection Layer"]} Non-Learnable, Scale={trial['Projection Arguments']['scale']}"
+                names.append(name)
 
             # Create a 95% confidence interval for the mean.
             try:
@@ -54,14 +59,15 @@ def plot(matrix, names, outfile=None):
     for i, table in enumerate(matrix):
         x = np.arange(1, table.shape[1]+1)
         sns.lineplot(x=x, y=table[0,:], marker='', label= names[i])
-        if args.CI == "True":
+        if args.CI:
             plt.fill_between(x, table[1,:], table[2,:], alpha=0.3)
 
     plt.title(args.filepath)
     plt.xlabel('Epochs')
     plt.ylabel(args.yaxis)
     plt.xticks(x)
-    plt.legend()
+    if len(names) != 0:
+        plt.legend()
     if args.ylim is not None:
         plt.ylim(args.ylim[0], args.ylim[1])
     if outfile != None:
@@ -73,8 +79,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot test accuracy from pickle files.")
     parser.add_argument('--filepath', type=str, default="Results/ff_rks_l_lr.json", help="Path to json file.")
     parser.add_argument('--yaxis', type=str, default="Test Accuracies", help="What to plot on the y-axis.")
-    parser.add_argument('--CI', type=str, default="True", help="Plot confidence intervals or not.")
-    parser.add_argument('--ylim', nargs='+', default=None, type=int, help='Upper and Lower limits for the plot.')
+    parser.add_argument('--ylim', nargs='+', default=None, type=float, help='Upper and Lower limits for the plot.')
+    parser.add_argument('--CI', action='store_true', help="Plot confidence intervals or not.")
+    parser.add_argument('--rks', action='store_true', help='Plot Random Kitchen Sink trials')
+    parser.add_argument('--ff', action='store_true', help='Plot Fastfood trials')
     args = parser.parse_args()
 
     parse_json(args.filepath, args.yaxis)
